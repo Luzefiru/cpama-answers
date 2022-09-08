a2184d4d9b3d80a0a5f8fcdbbe9f566f39bdaee7
Luzefiru <105530193+Luzefiru@users.noreply.github.com>
2022-09-08 21:46:14 +0800

    Add files via upload
    This chapter focused on printf & scanf functions. It also talked about minimum field widths & precision specifiers. 
    
    It also goes in-depth on how the compiler reads input data and how it uses a hidden buffer to do it.

---
+++ 3_5.c
@@ -0,0 +1,19 @@
+/*
+I predict the answer to be:
+12.3 45 0.600000
+
+[✓]
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    float x, y;
+
+    scanf("%f%d%f", &x, &i, &y);
+    printf("%f %d %f", x, i, y);
+
+    return 0;
+}
