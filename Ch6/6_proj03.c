135c05746e83ef576e8211c5c067243ffcff3db6
Luzefiru <ian.deejay.iv@gmail.com>
2022-09-19 10:34:56 +0800

    Added 6_proj03.c

---
+++ Ch6/6_proj03.c
@@ -0,0 +1,46 @@
+/* program that reduces an inputted fraction to lowest terms using Euclidean's Algorithm */
+
+#include <stdio.h>
+
+int fractionGCD();
+
+int main(void)
+{
+    int numerator, denominator;
+
+    // asks for the fraction input in the form (n/d)
+    printf("Enter a fraction (n/d): ");
+    scanf("%d/%d", &numerator, &denominator);
+
+    // reduces & displays the fraction in lowest terms
+    printf("In lowest terms: %d/%d",
+    numerator / fractionGCD(numerator, denominator),
+    denominator / fractionGCD(numerator, denominator));
+
+    return 0;
+}
+
+/**
+ * @brief Finds the GCD of a fraction of integers.
+ * 
+ * @param n (int) : the numerator of the fraction
+ * @param d (int) : the denominator of the fraction
+ * @return (int) : the GCD of the fraction
+ */
+int fractionGCD(int n, int d)
+{
+    // initializes variable for remainder to be used in Euclidean's Algorithm
+    int r;
+
+    // executes Euclidean's Algorithm
+    while (n != 0)
+    {
+        r = d % n;
+        d = n;
+        n = r;
+    }
+
+    // returns the GCD
+    return d;
+
+}
