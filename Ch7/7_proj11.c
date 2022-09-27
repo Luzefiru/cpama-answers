ce0d29fe105d226670291c2d75c059640f34ad18
Luzefiru <ian.deejay.iv@gmail.com>
2022-09-27 20:27:55 +0800

    Fixed 7_proj11.c

--- Ch7/7_proj11.c
+++ Ch7/7_proj11.c
@@ -1,4 +1,7 @@
-/* takes an inputted first name & last name, then displays it in this format: "lastName, firstNameInitial." */
+/*
+takes an inputted first name & last name, then displays it in this format: "lastName, firstNameInitial."
+ignores spaces before the first name, in between both names, and after last name.
+*/
 
 #include <stdio.h>
 
@@ -7,15 +10,21 @@ int main(void)
     char ch, fn_initial;
 
     printf("Enter a first and last name (firstName lastName): ");
-    scanf("%c", &fn_initial); // saves the firstNameInitial
+    
+    while (ch = getchar(), ch == ' ') ; // strips all spaces before characters
 
-    while (ch = getchar(), ch != ' ') // ignores all characters up until the space between the first & last names
-        ;
+    fn_initial = ch; // saves the first name initial
 
-    while (ch = getchar(), ch != '\n') // prints out all characters of the last name first
+    while (ch = getchar(), ch != ' ') ; // strips all letters
+    while (ch = getchar(), ch == ' ') ; // strips all spaces
+
+    printf("%c", ch); // prints the first letter of the last name
+
+    while (ch = getchar(), ch != '\n' && ch != ' ') // prints the rest of the last name, ignoring extra spaces
         printf("%c", ch);
     
     printf(", %c.", fn_initial); // appends the ", {fn_initial}." to the last name
 
     return 0;
 }
+
