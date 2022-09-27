4b643741e1d9042947f397dd87cb9a0e47753d9f
Luzefiru <ian.deejay.iv@gmail.com>
2022-09-27 21:36:45 +0800

    Added 7_proj14.c

---
+++ Ch7/7_proj14.c
@@ -0,0 +1,23 @@
+/* uses Newton-Raphson algorithm to find the square root of a number */
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    // y is initial guess for the square root, epsilon is the allowable treshold
+    double x, y = 1, avg, epsilon = 0.00001;
+
+    printf("Enter a positive number: ");
+    scanf("%lf", &x);
+
+    while (1)
+    {
+        avg = (y + x/y)/2;
+        if (fabs(y - avg) < 0.00001*y) break;
+        y = avg;
+
+    }
+
+    printf("Square root: %lf", avg);
+}
