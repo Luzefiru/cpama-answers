f8b699c985335313cda537a6fd5521a336dba9be
Luzefiru <ian.deejay.iv@gmail.com>
2022-11-07 15:23:59 +0800

    Added 16_ex20.c

---
+++ Ch16/16_ex20.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+enum {NORTH, SOUTH, EAST, WEST} direction;
+
+int main(void)
+{
+    int x = 0, y = 0;
+    direction = NORTH;
+
+    switch (direction)
+    {
+        case EAST:
+            x++; break;
+        case WEST:
+            x--; break;
+        case SOUTH:
+            y--; break;
+        case NORTH:
+            y++; break;
+    }
+
+    printf("%d %d", x, y);
+}
