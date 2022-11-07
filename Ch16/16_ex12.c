24ffd2d01fb7a9593e740e445c46aa6f1dde181a
Luzefiru <ian.deejay.iv@gmail.com>
2022-11-07 09:04:59 +0800

    Correction

--- Ch16/16_ex12.c
+++ Ch16/16_ex12.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-union // max possible = 8 bytes, so occupies 8 bytes
+union // max possible = 16 bytes, so occupies 16 bytes
 {
     double a; // 8 bytes:
-    struct // max possible: 8 bytes
+    struct // total bytes allocated = 16 bytes
     {
         char b[4];
         double c;
@@ -12,4 +12,4 @@ union // max possible = 8 bytes, so occupies 8 bytes
     char f[4]; // 4 bytes
 } u;
 
-// union 'u' occupies 8 bytes.
+// union 'u' occupies 16 bytes.
