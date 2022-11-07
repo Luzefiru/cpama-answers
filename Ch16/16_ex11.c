1b034579369134864a16d9eb41fd723bc536b505
Luzefiru <ian.deejay.iv@gmail.com>
2022-11-07 08:59:53 +0800

    Changed 16_ex11.c

--- Ch16/16_ex11.c
+++ Ch16/16_ex11.c
@@ -2,12 +2,14 @@
 
 struct
 {
-    double a;
-    union
+    double a; // 8 bytes
+    union // 8 bytes
     {
         char b[4];
         double c;
         int d;
     } e;
-    char f[4];
+    char f[4]; // 4 bytes
 } s;
+
+// s occupies 20 bytes.
